736cf24fc6f17cafc766b22658ddade553b7a97c
spacetomtom <64090639+spacetomtom@users.noreply.github.com>
2023-05-09 15:25:53 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,19 +1,29 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+// server.js
+var jsonServer = require('json-server')
+var server = jsonServer.create()
+var router = jsonServer.router('db.json')
+var middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
+
+server.use(jsonServer.bodyParser)
+server.use(function (req, res, next) {
+  if (req.method === 'POST') {
+    
+    // Converts POST to GET and move payload to query params
+    // This way it will make JSON Server that it's GET request
+    console.log("----------------------------------------------------------------")
+    console.log(req.body)
+    req.url='/prompt'+req.body.suggestionId
+    req.method = 'GET'
+    req.query = req.body
+}
+    
+  // Continue to JSON Server router
+  next()
 })
 
-// Export the Server API
-module.exports = server
+server.use(router)
+server.listen(3000, function () {
+  console.log('JSON Server is running')
+})
